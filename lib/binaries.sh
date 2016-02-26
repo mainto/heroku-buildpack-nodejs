b0bc0bb6332972fa50fe2a970aa51a14976ae702
Deokhyun Ko <mainto@gmail.com>
2016-02-26 22:24:14 +0100

    remove spaces

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -17,7 +17,7 @@ install_nodejs() {
   fi
 
   echo "Downloading and installing node $version..."
-  local versionfolder = $(echo $version| cut -d'.' -f 1,2)
+  local versionfolder=$(echo $version| cut -d'.' -f 1,2)
   local download_url="https://deb.nodesource.com/node_$versionfolder/pool/main/n/nodejs/nodejs_$version-1nodesource1~trusty1_armhf.deb
   curl "$download_url" --silent --fail  --retry 5 --retry-max-time 15 -o /tmp/node.deb || (echo "Unable to download node $version; does it exist?" && false)
   rm -rf /tmp/node
@@ -39,7 +39,7 @@ install_iojs() {
   fi
 
   echo "Downloading and installing iojs $version..."
-  local versionfolder = $(echo $version| cut -d'.' -f 1)
+  local versionfolder=$(echo $version| cut -d'.' -f 1)
   local download_url="https://deb.nodesource.com/iojs_$versionfolder.x/pool/main/i/iojs/iojs_$version-1nodesource1~trusty1_armhf.deb
   curl "$download_url" --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.deb || (echo "Unable to download iojs $version; does it exist?" && false)
   rm -rf /tmp/node
